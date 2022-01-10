c1b315884c85bcdf344740f38df64cc50a758200
Mesum Abbas <syedmesum387@gmail.com>
2022-01-10 13:02:32 +0500

    timer

---
+++ app.js
@@ -0,0 +1,23 @@
+var myhours = 0;
+var mymin = 0;
+var mysec = 0;
+var displayhrs = document.getElementById("hrs");
+var displaymins = document.getElementById("mins");
+var displaysec = document.getElementById("sec");
+
+
+function startbtn(){
+    
+}
+function stopbtn(){
+
+}
+function resetbtn(){
+
+displayhrs.innerHTML = myhours;
+displaymins.innerHTML = mymins;
+displaysec.innerHTML = mysec;
+myhours = 0;
+mymins = 0;
+mysec = 0;
+}
